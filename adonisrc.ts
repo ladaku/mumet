986371d74162fa184f0e7c50518e93ef62c960de
ladaku <bawangayam69@gmail.com>
2025-03-04 06:32:50 +0700

    mn

--- adonisrc.ts
+++ adonisrc.ts
@@ -84,7 +84,7 @@ export default defineConfig({
   ],
 
   assetsBundler: false,
-  hooks: {
-    onBuildStarting: [() => import('@adonisjs/vite/build_hook')],
-  },
+  // hooks: {
+  //   onBuildStarting: [() => import('@adonisjs/vite/build_hook')],
+  // },
 })
